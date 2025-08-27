16772e4e000a8fe7915e88c8078ea1ee6b2333a6
jinyouli <229338934@qq.com>
2025-08-27 13:31:36 +0800

    AI完成

--- Game/AI/Decks/MikankoExecutor.cs
+++ Game/AI/Decks/MikankoExecutor.cs
@@ -13,47 +13,34 @@ namespace WindBot.Game.AI.Decks
    {
         public class CardId
         {
-          //main code
-          //陨石
-          public const int NibiruThePrimalBeing = 27204311;
-          //灰流丽
-          public const int AshBlossom = 14558127;
-          public const int NaturalBeast = 33198837;
-          public const int NaturalExterio = 99916754;
-          public const int ImperialOrder = 61740673;
-          public const int SwordsmanLV7 = 37267041;
-          public const int RoyalDecree = 51452091;
-          public const int Number41BagooskatheTerriblyTiredTapir = 90590303;
-          //增殖的g
-          public const int MaxxC = 23434538;
-          //撒旦老人
-          public const int OldMan = 46565218;
-          public const int AlbionTheShroudedDragon = 25451383;
-          public const int SkillDrain = 82732705;
+            //main code
+            public const int HuaishouQiailu = 63941210;
+            public const int Huaishoulongwang = 48770333;
+            public const int OldMan = 46565218;
+            public const int Yuwudarinv = 81260679;
+            public const int AshBlossom = 14558127;
+            public const int MiEarth = 11161666;
+            public const int MiRed = 18377261;
+            public const int MiGreen = 6327734;
+            public const int MaxxC = 23434538;
+            public const int Yuwuerguizi = 84550369;
+            public const int Yuwuchuancheng = 44649322;
+            public const int Yuwushuiwudao = 43527730;
+            public const int MikankoBird = 57736667;
+            public const int Yuwuhuocongwu = 80044027;
+            public const int Yuwushenyin = 53174748;
+            public const int Yuwushenjiao = 78199891;
 
-          //main code
-          public const int HuaishouQiailu = 63941210;
-          public const int Huaishoulongwang = 48770333;
-          public const int Rongyanmoshen = 102380;
-          
-          public const int MiGreen = 6327734;
-          public const int MiEarth = 11161666;
-          public const int MiRed = 18377261;
-          public const int Yuwudarinv = 81260679;
-          public const int Yuwuerguizi = 84550369;
-          public const int Yishizhunbei = 96729612;
-          public const int Yuwuchuancheng = 44649322;
-          public const int Hanyinkaijia = 33114323;
-          public const int Tianzizhilun = 40678060;
-          public const int Yuwushuiwudao = 43527730;
-          public const int MikankoBird = 57736667;
-          public const int Yuwuroulunwu = 79912449;
-          public const int Yuwuhuocongwu = 80044027;
-          public const int Yuwushenyin = 53174748;
-          public const int Yuwushenjiao = 78199891;
+            //extra code
+            public const int Yuwuguirinv = 57566760;
 
-          //extra code
-          public const int Yuwuguirinv = 57566760;
+            public const int NaturalExterio = 99916754;
+            public const int ImperialOrder = 61740673;
+            public const int SwordsmanLV7 = 37267041;
+            public const int RoyalDecree = 51452091;
+            public const int Number41BagooskatheTerriblyTiredTapir = 90590303;
+            public const int AlbionTheShroudedDragon = 25451383;
+            public const int SkillDrain = 82732705;
 
         }
         
@@ -71,36 +58,23 @@ namespace WindBot.Game.AI.Decks
             //counter
             AddExecutor(ExecutorType.Activate, CardId.AshBlossom, AshBlossomActivate);
             AddExecutor(ExecutorType.Activate, CardId.MaxxC, MaxxCActivate);
-            AddExecutor(ExecutorType.Activate, CardId.NibiruThePrimalBeing, NibiruThePrimalBeingActivate);
             AddExecutor(ExecutorType.Activate, CardId.MikankoBird, MikankoBirdEffect);
-
-            AddExecutor(ExecutorType.Activate, CardId.NaturalBeast , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.NaturalExterio , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.ImperialOrder , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.SwordsmanLV7 , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.RoyalDecree , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.OldMan , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.AlbionTheShroudedDragon , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.SkillDrain , DefaultField);
+
             AddExecutor(ExecutorType.Activate, CardId.HuaishouQiailu , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.Huaishoulongwang , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.Rongyanmoshen , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.MiGreen , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.MiEarth , MiEarthEffect);
             AddExecutor(ExecutorType.Activate, CardId.MiRed , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.Yuwudarinv , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.Yuwuerguizi , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.Yishizhunbei , DefaultField);
+
             AddExecutor(ExecutorType.Activate, CardId.Yuwuchuancheng , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.Hanyinkaijia , SpellActive);
-            AddExecutor(ExecutorType.Activate, CardId.Tianzizhilun , SpellActive);
             AddExecutor(ExecutorType.Activate, CardId.Yuwushuiwudao , SpellActive);
-            AddExecutor(ExecutorType.Activate, CardId.Yuwuroulunwu , SpellActive);
             AddExecutor(ExecutorType.Activate, CardId.Yuwuhuocongwu , SpellActive);
             AddExecutor(ExecutorType.Activate, CardId.Yuwushenyin , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.Yuwushenjiao , DefaultField);
             AddExecutor(ExecutorType.Activate, CardId.Yuwuguirinv , DefaultField);
-            AddExecutor(ExecutorType.Activate, CardId.Number41BagooskatheTerriblyTiredTapir , DefaultField);
 
 
             AddExecutor(ExecutorType.SpSummon);
@@ -109,7 +83,6 @@ namespace WindBot.Game.AI.Decks
             AddExecutor(ExecutorType.Summon);
             AddExecutor(ExecutorType.Summon, CardId.AshBlossom, AshBlossomSummon);
             AddExecutor(ExecutorType.Summon, CardId.OldMan, OldManSummon);
-            AddExecutor(ExecutorType.Summon, CardId.NibiruThePrimalBeing, NibiruThePrimalBeingSummon);
             AddExecutor(ExecutorType.Summon, CardId.MaxxC, MaxxCSummon);
 
         }
@@ -124,11 +97,6 @@ namespace WindBot.Game.AI.Decks
         }
 
         private bool MaxxCSummon()
-        {
-            return false;
-        }
-
-        private bool NibiruThePrimalBeingSummon()
         {
             return false;
         }
@@ -151,7 +119,7 @@ namespace WindBot.Game.AI.Decks
                 List<ClientCard> enemy_monsters = Enemy.GetMonsters();
                 foreach(ClientCard card in enemy_monsters)
                 {
-                    if (card != null && !card.IsCode(CardId.Huaishoulongwang) && !card.IsCode(CardId.HuaishouQiailu) && !card.IsCode(CardId.Rongyanmoshen) && !card.IsCode(CardId.OldMan))
+                    if (card != null && !card.IsCode(CardId.Huaishoulongwang) && !card.IsCode(CardId.HuaishouQiailu) && !card.IsCode(CardId.OldMan))
                     {
                         target = card;
                     }
@@ -288,7 +256,6 @@ namespace WindBot.Game.AI.Decks
             if (Enemy.HasInMonstersZone(CardId.NaturalExterio, true) && !isCounter) return true;
             if (target.IsSpell())
             {
-                if (Enemy.HasInMonstersZone(CardId.NaturalBeast, true)) return true;
                 if (Enemy.HasInSpellZone(CardId.ImperialOrder, true) || Bot.HasInSpellZone(CardId.ImperialOrder, true)) return true;
                 if (Enemy.HasInMonstersZone(CardId.SwordsmanLV7, true) || Bot.HasInMonstersZone(CardId.SwordsmanLV7, true)) return true;
             }
@@ -303,42 +270,6 @@ namespace WindBot.Game.AI.Decks
         public bool CheckNumber41(ClientCard card)
         {
             return card != null && card.IsFaceup() && card.IsCode(CardId.Number41BagooskatheTerriblyTiredTapir) && card.IsDefense() && !card.IsDisabled();
-        }
-
-        public bool NibiruThePrimalBeingActivate()
-        {
-            if (CheckWhetherNegated()) return false;
-            if (Duel.Player == 0 || Bot.GetMonsters().Any(card => card.IsFaceup() && card.HasType(CardType.Synchro)))
-            {
-                return false;
-            }
-            
-            if (Util.GetBestAttack(Enemy) > Util.GetBestAttack(Bot))
-            {
-                // end main phase
-                if ((CurrentTiming & hintTimingMainEnd) != 0)
-                {
-                    SelectNibiruPosition();
-                    return true;
-                }
-                
-                // avoid Baronne de Fleur
-                List<ClientCard> tunerList = Enemy.GetMonsters().Where(card => card.IsFaceup() && card.IsTuner() && !card.HasType(CardType.Xyz | CardType.Link)).ToList();
-                List<ClientCard> nonTunerList = Enemy.GetMonsters().Where(card => card.IsFaceup() && !card.IsTuner() && !card.HasType(CardType.Xyz | CardType.Link)).ToList();
-                foreach (ClientCard tuner in tunerList)
-                {
-                    foreach (ClientCard nonTuner in nonTunerList)
-                    {
-                        if (tuner.Level + nonTuner.Level == 10)
-                        {
-                            SelectNibiruPosition();
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
         }
 
         public void SelectNibiruPosition()
@@ -426,46 +357,37 @@ namespace WindBot.Game.AI.Decks
         {
             switch (id)
             {
-                case CardId.NibiruThePrimalBeing:
-                    return Bot.GetRemainingCount(CardId.NibiruThePrimalBeing, 1);
                 case CardId.HuaishouQiailu:
-                    return Bot.GetRemainingCount(CardId.HuaishouQiailu, 1);
+                    return Bot.GetRemainingCount(CardId.HuaishouQiailu, 2);
                 case CardId.Huaishoulongwang:
-                    return Bot.GetRemainingCount(CardId.Huaishoulongwang, 1);
-                case CardId.Rongyanmoshen:
-                    return Bot.GetRemainingCount(CardId.Rongyanmoshen, 1);
+                    return Bot.GetRemainingCount(CardId.Huaishoulongwang, 2);
                 case CardId.OldMan:
-                    return Bot.GetRemainingCount(CardId.OldMan, 1);
+                    return Bot.GetRemainingCount(CardId.OldMan, 2);
                 case CardId.AshBlossom:
-                    return Bot.GetRemainingCount(CardId.AshBlossom, 3);
+                    return Bot.GetRemainingCount(CardId.AshBlossom, 2);
                 case CardId.MiGreen:
-                    return Bot.GetRemainingCount(CardId.MiGreen, 2);
+                    return Bot.GetRemainingCount(CardId.MiGreen, 3);
                 case CardId.MiEarth:
-                    return Bot.GetRemainingCount(CardId.MiEarth, 2);
+                    return Bot.GetRemainingCount(CardId.MiEarth, 3);
                 case CardId.MiRed:
                     return Bot.GetRemainingCount(CardId.MiRed, 2);
                 case CardId.MaxxC:
-                    return Bot.GetRemainingCount(CardId.MaxxC, 3);
+                    return Bot.GetRemainingCount(CardId.MaxxC, 2);
                 case CardId.Yuwudarinv:
-                    return Bot.GetRemainingCount(CardId.Yuwudarinv, 2);
+                    return Bot.GetRemainingCount(CardId.Yuwudarinv, 3);
                 case CardId.Yuwuerguizi:
                     return Bot.GetRemainingCount(CardId.Yuwuerguizi, 2);
-                case CardId.Yishizhunbei:
-                    return Bot.GetRemainingCount(CardId.Yishizhunbei, 2);
+
                 case CardId.Yuwuchuancheng:
-                    return Bot.GetRemainingCount(CardId.Yuwuchuancheng, 2);
-                case CardId.Hanyinkaijia:
-                    return Bot.GetRemainingCount(CardId.Hanyinkaijia, 2);
-                case CardId.Tianzizhilun:
-                    return Bot.GetRemainingCount(CardId.Tianzizhilun, 1);
+                    return Bot.GetRemainingCount(CardId.Yuwuchuancheng, 1);
+
                 case CardId.Yuwushuiwudao:
-                    return Bot.GetRemainingCount(CardId.Yuwushuiwudao, 2);
+                    return Bot.GetRemainingCount(CardId.Yuwushuiwudao, 3);
                 case CardId.MikankoBird:
                     return Bot.GetRemainingCount(CardId.MikankoBird, 2);
-                case CardId.Yuwuroulunwu:
-                    return Bot.GetRemainingCount(CardId.Yuwuroulunwu, 1);
+
                 case CardId.Yuwuhuocongwu:
-                    return Bot.GetRemainingCount(CardId.Yuwuhuocongwu, 1);
+                    return Bot.GetRemainingCount(CardId.Yuwuhuocongwu, 2);
                 case CardId.Yuwushenyin:
                     return Bot.GetRemainingCount(CardId.Yuwushenyin, 1);
                 case CardId.Yuwushenjiao:
